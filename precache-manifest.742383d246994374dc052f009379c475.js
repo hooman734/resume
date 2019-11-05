17b583427e3ee909017356e06974f80e107d24a4
hooman777 <eng.hesamyan@gmail.com>
2019-11-06 00:29:10 +0400

    Updates

---
+++ precache-manifest.742383d246994374dc052f009379c475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45668a7fec8e2362f9010197141a5793",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "77e612d2220941829353",
+    "url": "/resume/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "37d15089530ba4449c14",
+    "url": "/resume/static/js/2.9b7d66df.chunk.js"
+  },
+  {
+    "revision": "77e612d2220941829353",
+    "url": "/resume/static/js/main.eaaaa235.chunk.js"
+  },
+  {
+    "revision": "431861597e9accc79666",
+    "url": "/resume/static/js/runtime-main.4ae24eb6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/resume/static/media/logo.25bf045c.svg"
+  }
+]);
