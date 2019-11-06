fa659dd0433d3453d6f2b53637e1915bc09d203e
hooman777 <eng.hesamyan@gmail.com>
2019-11-07 02:53:28 +0400

    some commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Personal_import from "./components/Personal_info";
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar></Navbar>
-      <Personal_import></Personal_import>
+      <Navbar />
     </React.Fragment>
   );
 }
